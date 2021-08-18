85a013b94beeb1385a5f5d0548f7ed475814edef
etamar <etamar1122@gmail.com>
2021-08-19 02:41:17 +0300

    server + sql ready + some mobile css

--- main.js
+++ main.js
@@ -79,3 +79,28 @@ $(".header-text ").css('display','none');
 $("#signup-now").css('display','none');
 $(document).ready(setTimeout(function(){$(".header-text").fadeIn()},200));
 $(document).ready(setTimeout(function(){$("#signup-now").fadeIn()},200));
+
+
+
+
+
+
+function initMap(): void {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(
+      document.getElementById("map") as HTMLElement,
+      {
+        zoom: 4,
+        center: uluru,
+      }
+    );
+  
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+                       
